16a50bfc77336e2512f557245609a4d52ce8b7db
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-11-08 20:03:38 +0100

    feat: add calculator to MainActivity

--- app/src/main/java/com/m3lnyk/simple_calculator/MainActivity.kt
+++ app/src/main/java/com/m3lnyk/simple_calculator/MainActivity.kt
@@ -3,14 +3,13 @@ package com.m3lnyk.simple_calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.m3lnyk.simple_calculator.ui.theme.MediumGray
 import com.m3lnyk.simple_calculator.ui.theme.SimpleCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,13 +20,17 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<CalculatorViewModel>()
                 val state = viewModel.state
                 val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-
-}
