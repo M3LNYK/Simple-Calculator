1ecc4c33c188017f9cfdb626fe7c0d533359de82
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-11-08 18:01:24 +0100

    feat: add CalculatorViewModel.kt

---
+++ app/src/main/java/com/m3lnyk/simple_calculator/CalculatorViewModel.kt
@@ -0,0 +1,12 @@
+package com.m3lnyk.simple_calculator
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+
+    var state by mutableStateOf(CalculatorState())
+        private set
+}
