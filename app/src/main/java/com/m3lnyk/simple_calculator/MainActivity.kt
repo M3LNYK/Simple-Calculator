a0e843f2f7870d1dd8d494d051465c1b51d2d6f1
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-11-08 21:06:08 +0100

    refactor: add preview to MainActivity

--- app/src/main/java/com/m3lnyk/simple_calculator/MainActivity.kt
+++ app/src/main/java/com/m3lnyk/simple_calculator/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.m3lnyk.simple_calculator.ui.theme.MediumGray
@@ -34,3 +36,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+
+}
