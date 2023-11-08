59e2e8e230b60af18fb89ffebdde00cc38f0c973
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-11-08 18:17:01 +0100

    feat: add viewModel and state to Main

--- app/src/main/java/com/m3lnyk/simple_calculator/MainActivity.kt
+++ app/src/main/java/com/m3lnyk/simple_calculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.m3lnyk.simple_calculator.ui.theme.SimpleCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleCalculatorTheme {
-
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
             }
         }
     }
